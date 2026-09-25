0177f8fd39d573e30dde9117c2738f4889bfd7f4
Andy Ripley <andy@ley.rip>
2026-09-24 21:53:30 -0400

    init

---
+++ src/components/retro/index.ts
@@ -0,0 +1,11 @@
+// Retro components. Auto-injected into every MDX post (see
+// src/pages/blog/[...slug].astro), so posts can use <Blink> etc. without imports.
+export { default as Badges } from "./Badges.astro";
+export { default as Blink } from "./Blink.astro";
+export { default as CursorTrail } from "./CursorTrail.astro";
+export { default as HitCounter } from "./HitCounter.astro";
+export { default as Marquee } from "./Marquee.astro";
+export { default as New } from "./New.astro";
+export { default as Rainbow } from "./Rainbow.astro";
+export { default as UnderConstruction } from "./UnderConstruction.astro";
+export { default as WebRing } from "./WebRing.astro";
